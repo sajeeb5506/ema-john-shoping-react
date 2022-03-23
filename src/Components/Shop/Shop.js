d0c2b0fa91a6ae62b4bc83e27a7f71d7fc0850c4
sajeeb5506 <sajeebsarker5506@gmail.com>
2022-03-23 14:27:36 +0600

    added a new components

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 
 import Product from '../Products/Product';
 import "./Shop.css"
@@ -29,8 +30,8 @@ const Shop = () => {
              }
             </div>
             <div className="card-container">
-            <h2>Order Sumary</h2>
-            <p>Selected Item: {cart.length}</p>
+          <Cart cart={cart}></Cart>
+         
             </div>
         </div>
     );
