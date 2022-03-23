1095bad60b67aaffa6204eec4a9a73cbb9615eac
sajeeb5506 <sajeebsarker5506@gmail.com>
2022-03-23 15:20:05 +0600

    oder summary calculate

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Cart.css';
-const Cart = (cart) => {
+const Cart = ({cart}) => {
+    let total = 0;
+    let shiping =0;
+     for(const product of cart){
+          total=total+product.price;
+          shiping=shiping+product.shipping;
+     }
+  const tax = (total *0.1).toFixed(2)  ;
     return (
-        <div>
+        <div className='cart'>
               <h2>Order Sumary</h2>
-            <p>Selected Item: {cart.length}</p>
+             <p>Selected Item: {cart.length}</p>
+             <p>Total price:${total} </p>
+             <p>Total shopping:{shiping}</p>
+             <p>Tax:{tax}</p>
+             <p className='bold'> Grand Total: </p>
         </div>
     );
 };
